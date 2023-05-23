251217633ed999cc3d7b6e3b30d845e543ef7fd7
Dennis Cai <dennis@thecodinganalyst.com>
2023-05-23 22:26:25 +0800

    add validation for products

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,6 +1,15 @@
+import { IsDecimal, IsString } from 'class-validator';
+
 export class CreateProductDto {
+  @IsString()
   name: string;
+
+  @IsString()
   description: string;
+
+  @IsDecimal()
   price: number;
+
+  @IsString()
   currency: string;
 }
