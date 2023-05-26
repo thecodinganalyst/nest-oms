4daa9ddf66ec724131445cf78718ba77cdbeec26
Dennis Cai <dennis@thecodinganalyst.com>
2023-05-26 17:03:59 +0800

    provide jwt token upon login success

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { LocalStrategy } from './local.strategy';
 import { CustomersModule } from '../customers/customers.module';
 import { PassportModule } from '@nestjs/passport';
+import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [CustomersModule, PassportModule],
-  providers: [LocalStrategy],
+  imports: [
+    CustomersModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '60s' },
+      }),
+    }),
+  ],
+  providers: [LocalStrategy, AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}
